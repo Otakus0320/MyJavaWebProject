a32d06a3ecfa15a29102ead5e4ac15286f058656
Otakus0320 <jioboyang@gmail.com>
2024-08-30 15:41:07 +0800

    initialize

---
+++ src/test/java/com/otakus0320/MyJavaWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otakus0320;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
